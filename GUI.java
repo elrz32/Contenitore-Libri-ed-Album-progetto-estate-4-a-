0d1b7302f1cb7b63f9b28402003d23a7daaf165c
elia <you@example.com>
2022-06-28 23:52:41 +0100

    ultimi minuti del 28/06/2022--> messi i campi su cui scrivere titolo autore ecc. + fatta transizione da menu principale a visita dei album/libri e della creazione di nuovi elementi.

--- GUI.java
+++ GUI.java
@@ -10,10 +10,17 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 public class GUI extends JFrame{
 
     private JPanel contenitore,nord,sud,centro;
     private BufferedImage contenitoreImmagine;
+    private JLabel scrittaNord,scrittaSud, tabellaMostraImmagine;
+    private JButton ritornoHome,catalogoLibri,catalogoAlbum,aggiungiLibro,creaLibro,aggiungiAlbum,creaAlbum;
+    private boolean immagineMessa;
+    private JTextField titolo,autore,anno,numeroPagine,commento,numeroMusiche,genere;
+    //private ??? voto (va 1 a 5 stelle) TODO
 
     public GUI(){
         this.setTitle("contenitore album ecc");
@@ -35,60 +42,193 @@ public class GUI extends JFrame{
         contenitore.add(sud,BorderLayout.SOUTH);
         contenitore.add(centro,BorderLayout.CENTER);
 
+        scrittaNord=new JLabel();
+        scrittaSud=new JLabel();
+        ritornoHome=new JButton();
+        catalogoAlbum=new JButton();
+        catalogoLibri=new JButton();
+        aggiungiLibro=new JButton();
+        creaLibro=new JButton();
+        aggiungiAlbum=new JButton();
+        creaAlbum=new JButton();
+        tabellaMostraImmagine=new JLabel();
+        titolo=new JTextField(30);
+        autore=new JTextField(30);
+        anno=new JTextField(30);
+        genere=new JTextField(30);
+        numeroMusiche=new JTextField(30);
+        numeroPagine=new JTextField(30);
+        commento=new JTextField(30);
+        immagineMessa=false;
         homePage();
         this.add(contenitore);
         this.setVisible(true);
     }
 
     public void homePage(){
-        nord.add(new JLabel("CONTENITORE DI ALBUM E DI LIBRI"));
+        catalogoAlbum.setVisible(true);
+        catalogoLibri.setVisible(true);
+        ritornoHome.setVisible(false);
+        aggiungiLibro.setVisible(false);
+        aggiungiAlbum.setVisible(false);
+        creaLibro.setVisible(false);
+        titolo.setVisible(false);
+        autore.setVisible(false);
+        anno.setVisible(false);
+        genere.setVisible(false);
+        numeroMusiche.setVisible(false);
+        commento.setVisible(false);
+        numeroPagine.setVisible(false);
+
+        scrittaNord.setText("CONTENITORE DI ALBUM E DI LIBRI");
+        scrittaNord.setVisible(true);
+        nord.add(scrittaNord);
         nord.setBackground(Color.CYAN);
-        sud.add(new JLabel("By Elia Rizzi"));
+
+        scrittaSud.setText("By Rizzi Industries");
+        scrittaSud.setVisible(true);
+        sud.add(scrittaSud);
         sud.setBackground(Color.GRAY);
+
         centro.setLayout(new FlowLayout());
         centro.setBackground(Color.LIGHT_GRAY);
-        JButton catalogoLibri=new JButton("clicca qui per vedere il tuo catalogo dei libri");
-        //TODO collegamento pulsante-schermataLibri
-        JButton catalogoAlbum=new JButton("clicca qui per vedere il catalogo dei tuoi album");
-        //TODO collegamento pulsante-schermataAlbum
+
+        catalogoLibri.setText("clicca qui per vedere il tuo catalogo dei libri");
+        catalogoLibri.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                tabellaMostraImmagine.setVisible(false);
+                catalogoAlbum.setVisible(false);
+                catalogoLibri.setVisible(false);
+                mostraLibri();
+            }
+        });
+
+        catalogoAlbum.setText("clicca qui per vedere il catalogo dei tuoi album");
+        catalogoAlbum.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                tabellaMostraImmagine.setVisible(false);
+                catalogoAlbum.setVisible(false);
+                catalogoLibri.setVisible(false);
+                mostraAlbum();
+            }
+        });
         //come mettere un'immagine
         try {
-            contenitoreImmagine=ImageIO.read(new File(".\\src\\Dati\\Immagini\\contenitore.png"));
-            JLabel tabellaMostraImmagine=new JLabel(new ImageIcon(contenitoreImmagine));
-            centro.add(tabellaMostraImmagine);
+            if(immagineMessa==false){
+                contenitoreImmagine=ImageIO.read(new File(".\\src\\Dati\\Immagini\\contenitore.png"));
+                tabellaMostraImmagine=new JLabel(new ImageIcon(contenitoreImmagine));
+                centro.add(tabellaMostraImmagine);
+                immagineMessa=true;
+            }
         } catch (Exception e) {
             System.err.println("errore nel caricamento dell'immagine");
         }
+        tabellaMostraImmagine.setVisible(true);
         centro.add(catalogoLibri);
         centro.add(catalogoAlbum);
     }
 
     public void mostraLibri(){
-        nord.add(new JLabel("Elenco dei libri presenti"));
-        JButton ritornoHome=new JButton("Torna alla schermata Home"); 
+        scrittaNord.setText("Elenco dei libri presenti");
+        ritornoHome.setVisible(true);
+        ritornoHome.setText("Torna alla schermata Home"); 
+        ritornoHome.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                homePage();
+            }
+        });
         sud.add(ritornoHome);
+        //pulsante per l'aggiunta di un libro
+        aggiungiLibro.setText("Clicca qui per aggiungere un libro!");
+        aggiungiLibro.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                creaLibro();
+            }
+        });
+        aggiungiLibro.setVisible(true);
+        centro.add(aggiungiLibro);
     }
 
     public void mostraAlbum(){
-        nord.add(new JLabel("Elenco degli album presenti"));
-        JButton ritornoHome=new JButton("Torna alla schermata Home");
+        scrittaNord.setText("Elenco degli album presenti");
+        ritornoHome.setText("Torna alla schermata Home");
+        ritornoHome.setVisible(true);
+        ritornoHome.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                homePage();
+            }
+        });
         sud.add(ritornoHome);
+        //pulsante per l'aggiunta di un album
+        aggiungiAlbum.setVisible(true);
+        aggiungiAlbum.setText("Clicca qui per aggiungere un album!");
+        aggiungiAlbum.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                creaAlbum();
+            }
+        });
+        aggiungiAlbum.setVisible(true);
+        centro.add(aggiungiAlbum);
     }
 
+    public void creaLibro(){
+        titolo.setVisible(true);
+        autore.setVisible(true);
+        anno.setVisible(true);
+        genere.setVisible(true);
+        commento.setVisible(true);
+        numeroPagine.setVisible(true);
+        aggiungiLibro.setVisible(true);
+        scrittaNord.setText("Inserisci i dati del tuo libro");
+        creaLibro.setText("aggiungi il libro");
+        creaLibro.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO immetto il libro nella lista
+            }
+        });
+        creaLibro.setVisible(true);
+        sud.add(creaLibro);
+        //TODO schermata di aggiunta libro
+        centro.add(titolo);
+        centro.add(autore);
+        centro.add(anno);
+        centro.add(genere);
+        centro.add(numeroPagine);
+        centro.add(commento);
+    }
 
+    public void creaAlbum(){
+        titolo.setVisible(true);
+        autore.setVisible(true);
+        anno.setVisible(true);
+        genere.setVisible(true);
+        numeroMusiche.setVisible(true);
+        commento.setVisible(true);
 
-    //Ascoltatori per i pulsanti
-    public  class SchermataCatalogoLibri implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			mostraLibri();
-		} 
-	}
-
-    public  class SchermataCatalogoAlbum implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			mostraAlbum();
-		} 
-	}
+        aggiungiAlbum.setVisible(false);
+        scrittaNord.setText("Inserisci i dati del tuo album");
+        creaAlbum.setText("aggiungi l'album");
+        creaAlbum.addActionListener(new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO immetto l'album nella lista
+            }
+        });
+        creaAlbum.setVisible(true);
+        sud.add(creaAlbum);
+        //TODO schermata aggiunta album
+        centro.add(titolo);
+        centro.add(autore);
+        centro.add(anno);
+        centro.add(genere);
+        centro.add(numeroMusiche);
+        centro.add(commento);
+    }
 }
